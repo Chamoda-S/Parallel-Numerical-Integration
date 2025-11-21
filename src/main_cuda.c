9b6f7b5537ea3cbb76167c153f6d19b908a36b21
Chamoda-S <chamoda.suraweera@gmail.com>
2025-11-21 10:41:34 +0530

    Add CUDA stub main

---
+++ src/main_cuda.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    (void)argc; (void)argv;
+    printf("CUDA version not implemented in this repository.\n");
+    printf("If you want, I can add a CUDA implementation (requires CUDA toolkit).\n");
+    return 0;
+}
